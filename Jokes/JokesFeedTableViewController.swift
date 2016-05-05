71be47d590b3cf01072f1b74dcb1301acabc9548
Abdelrahman Mohamed <obada.semary@gmail.com>
2016-05-05 11:06:38 +0300

    2.1 Fix

--- Jokes/JokesFeedTableViewController.swift
+++ Jokes/JokesFeedTableViewController.swift
@@ -36,11 +36,6 @@ class JokesFeedTableViewController: UITableViewController {
         })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
